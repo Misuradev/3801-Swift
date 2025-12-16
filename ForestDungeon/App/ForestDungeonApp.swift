7c0d7a40603d80d65eda177fe88ed7e68e49dbdf
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-16 11:34:28 +0200

    init

---
+++ ForestDungeon/App/ForestDungeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestDungeonApp.swift
+//  ForestDungeon
+//
+//  Created by Serhii Babchuk on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ForestDungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FDLaunchView()
+        }
+    }
+}
